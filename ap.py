35362dd28966ab6a05bec030e8eff31ed1154d9d
Mohammed Rahman Sherif <61545007+Mohammed-Rahman-sherif@users.noreply.github.com>
2023-04-06 14:59:39 +0530

    Add files via upload

---
+++ ap.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request, Form, Response
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
+import glob
+
+directory_path = 'Media/Result/'
+
+ap = FastAPI()
+ap.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates/")
+
+@ap.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index1.html", {"request": request})
+
+from fastapi.responses import FileResponse
+
+@ap.get('/result')
+def result_mp4():
+    video_path = "Media/Result/ZuYuY.mp4"
+    return FileResponse(video_path, media_type='video/mp4', filename='video.mp4')
